8ef57dac9c103d0d73b401cb51d6c19048ddb129
Mehdi <mehdichair7@gmail.com>
2024-07-18 02:47:45 +0200

    this is a feature commit

--- src/QuantityObserver.java
+++ src/QuantityObserver.java
@@ -1,3 +1,12 @@
 public class QuantityObserver implements OrderObserver {
-
+    @Override
+    public void update(Order order) {
+        if (order.getCount() > 5) {
+            order.setShippingCost(0);
+            System.out.println("QuantityObserver: Shipping cost set to zero. Current shipping cost: " + order.getShippingCost());
+        } else {
+            order.setShippingCost(10);
+            System.out.println("QuantityObserver: Shipping cost set to $10. Current shipping cost: " + order.getShippingCost());
+        }
+    }
 }
