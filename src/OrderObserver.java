de2fd35f3233072035a93989291a2334f6667dc5
Mehdi <mehdichair7@gmail.com>
2024-07-18 02:47:45 +0200

    this is a feature commit

--- src/OrderObserver.java
+++ src/OrderObserver.java
@@ -1,2 +1,3 @@
 public interface OrderObserver {
+    void update(Order order);
 }
--- src/PriceObserver.java
+++ src/PriceObserver.java
@@ -1,3 +1,9 @@
 public class PriceObserver implements OrderObserver {
-
+    @Override
+    public void update(Order order) {
+        if (order.getTotalPrice() > 200) {
+            order.setTotalPrice(order.getTotalPrice() - 20);
+            System.out.println("PriceObserver: Applied $20 discount. New total price: " + order.getTotalPrice());
+        }
+    }
 }
